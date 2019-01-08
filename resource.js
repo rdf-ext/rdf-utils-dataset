14b3884483c764f84261a59fcdb0c0899097e392
Victor Felder <victor@draft.li>
2019-01-08 17:52:33 +0100

    Lower resources time complexity

--- resource.js
+++ resource.js
@@ -1,17 +1,17 @@
 function resource (input, subject) {
   const quads = input.filter((quad) => {
     if (subject.termType === 'NamedNode') {
-      return quad.subject.value.split('#').shift() === subject.value.split('#').shift()
+      return quad.subject.value.split('#')[0] === subject.value.split('#')[0]
     }
 
     return quad.subject.equals(subject)
   })
 
   if (quads.length !== 0) {
-    quads.filter((quad) => {
-      return quad.object.termType !== 'NamedNode'
-    }).forEach((quad) => {
-      quads.addAll(resource(input, quad.object))
+    quads.forEach((quad) => {
+      if (quad.object.termType !== 'NamedNode') {
+        quads.addAll(resource(input, quad.object))
+      }
     })
   }
 
