7c70b18f1ed9ff4f60f31f93a55529b91dd8d8f5
Victor Felder <victor@draft.li>
2019-01-08 17:42:45 +0100

    Lower resourcesToGraph time complexity

--- resourcesToGraph.js
+++ resourcesToGraph.js
@@ -8,23 +8,26 @@ function resourcesToGraph (input, options) {
 
   const output = factory.dataset()
 
-  input.filter((quad) => {
-    return quad.subject.termType === 'NamedNode'
-  }).toArray().reduce((resourceIRIs, quad) => {
-    const resourceIRI = quad.subject.value.split('#').shift()
+  input.toArray()
+    .reduce((resourceIRIs, quad) => {
+      if (quad.subject.termType !== 'NamedNode') {
+        return resourceIRIs
+      }
+      const resourceIRI = quad.subject.value.split('#')[0]
 
-    if (resourceIRIs.indexOf(resourceIRI) === -1) {
-      resourceIRIs.push(resourceIRI)
-    }
+      if (resourceIRIs.indexOf(resourceIRI) === -1) {
+        resourceIRIs.push(resourceIRI)
+      }
 
-    return resourceIRIs
-  }, []).forEach((resourceIRI) => {
-    const resourceNode = factory.namedNode(resourceIRI)
+      return resourceIRIs
+    }, [])
+    .forEach((resourceIRI) => {
+      const resourceNode = factory.namedNode(resourceIRI)
 
-    output.addAll(resource(input, resourceNode).map((quad) => {
-      return factory.quad(quad.subject, quad.predicate, quad.object, resourceNode)
-    }))
-  })
+      output.addAll(resource(input, resourceNode).map((quad) => {
+        return factory.quad(quad.subject, quad.predicate, quad.object, resourceNode)
+      }))
+    })
 
   return output
 }
